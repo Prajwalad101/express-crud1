e475047c14c2c7e6f2969860565e2432f0f28e00
Prajwal Adhikari <prajwalad101@gmail.com>
2022-02-07 17:02:30 +0545

    add middleware to verify id

--- controller/studentController.js
+++ controller/studentController.js
@@ -4,6 +4,18 @@ const students = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/G2.json`)
 );
 
+const checkStId = (req, res, next, val) => {
+  const student = students.find((st) => st.id === val * 1);
+
+  if (!student) {
+    return res.status(400).json({
+      status: "fail",
+      message: "student not found",
+    });
+  }
+  next();
+};
+
 //* GETS ALL THE STUDENTS
 const getAllStudents = (req, res) => {
   res.status(200).json({
@@ -36,4 +48,17 @@ const createStudent = (req, res) => {
   );
 };
 
-module.exports = { getAllStudents, createStudent };
+//* GETS A STUDENT
+const getStudent = (req, res) => {
+  const id = req.params.id * 1;
+  const student = students.find((el) => el.id === id);
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      student,
+    },
+  });
+};
+
+module.exports = { getAllStudents, createStudent, getStudent, checkStId };
--- router/studentRoutes.js
+++ router/studentRoutes.js
@@ -3,9 +3,13 @@ const studentController = require("../controller/studentController");
 
 const router = express.Router();
 
+router.param("id", studentController.checkStId);
+
 router
   .route("/")
   .get(studentController.getAllStudents)
   .post(studentController.createStudent);
+
+router.route("/:id").get(studentController.getStudent);
 
 module.exports = router;
